1e0b228f0dea6b29369bd5c9b31f16da1e30538f
vinod10 <vinodkumar@kwiqpick.com>
2017-08-30 23:37:57 +0530

    added stylesheets for header and footer

--- wp-content/themes/b2w/footer.php
+++ wp-content/themes/b2w/footer.php
@@ -11,24 +11,12 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'b2w' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'b2w' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'b2w' ), 'b2w', '<a href="http://underscores.me/">VakaduVinod</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-3.2.1.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
 
 </body>
 </html>
